69befee5166ad59bf712f9add191bd3d4d56d08d
Lex Li <support@lextm.com>
2024-03-18 02:17:48 -0400

    Added a 6.0 release announcement.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,6 +85,7 @@ todo_include_todos = False
 # -- Options for HTML output ----------------------------------------------
 
 html_theme_options = {
+    'announcement': '<em>News:</em> PySNMP 6.0 is out! Learn more from <a href="https://www.pysnmp.com/pysnmp/upgrade">this page</a>.',
     'source_repository': 'https://github.com/lextudio/snmplabs.com',
     'source_branch': 'master',
     'source_directory': 'docs/source/',
