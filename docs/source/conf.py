c3a7172c6ab12aee7528f32ff242d1d3426cf3fb
Lex Li <support@lextm.com>
2026-03-13 07:11:42 -0400

    Update HTML announcement to promote SNMP Studio for Visual Studio Code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,7 @@ todo_include_todos = False
 # -- Options for HTML output ----------------------------------------------
 
 html_theme_options = {
-    'announcement': '<em>News:</em> PySNMP 7 is out! Learn more from <a href="https://www.pysnmp.com/pysnmp/upgrade">this page</a>.',
+    'announcement': 'Try <strong>SNMP Studio for Visual Studio Code</strong>, modern cross-platform SNMP workspace for professionals: <a href="https://www.sharpsnmp.com/#section-studio" target="_blank">Explore more</a>.',
     'source_repository': 'https://github.com/lextudio/snmplabs.com',
     'source_branch': 'master',
     'source_directory': 'docs/source/',
